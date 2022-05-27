d974cf810bd97716c6852da33dca1de51cbc0b9b
luaraRamos <luara.m.ramos@gmail.com>
2022-05-27 11:56:00 -0300

    Create ImcActivity

--- app/src/main/java/co/tiagoaguiar/codelab/myapplication/MainActivity.java
+++ app/src/main/java/co/tiagoaguiar/codelab/myapplication/MainActivity.java
@@ -1,14 +1,28 @@
 package co.tiagoaguiar.codelab.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    private View btnImc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnImc = findViewById(R.id.button_imc);
+
+        btnImc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ImcActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
