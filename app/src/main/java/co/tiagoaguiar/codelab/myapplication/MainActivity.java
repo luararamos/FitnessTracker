f49dca498e33436f5e3c97354775737e39e62b60
luaraRamos <luara.m.ramos@gmail.com>
2022-05-31 14:33:42 -0300

    Logic RecyclerView in MainActivity

--- app/src/main/java/co/tiagoaguiar/codelab/myapplication/MainActivity.java
+++ app/src/main/java/co/tiagoaguiar/codelab/myapplication/MainActivity.java
@@ -1,25 +1,62 @@
 package co.tiagoaguiar.codelab.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private View btnImc;
+    private RecyclerView rvMain;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnImc = findViewById(R.id.button_imc);
+        rvMain = findViewById(R.id.main_rv);
+        //1 -> Definir o comportamento de exibição do layout da recyclerview (mosaic, grid
+        // ou linear(horizontal/vertical))
+        rvMain.setLayoutManager(new LinearLayoutManager(this));
+        MainAdapter adapter = new MainAdapter();
+        rvMain.setAdapter(adapter);
+    }
 
-        btnImc.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, ImcActivity.class);
-            startActivity(intent);
-        });
+    private class MainAdapter extends RecyclerView.Adapter<MainViewHolder> {
+        @NonNull
+        @Override
+        public MainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new MainViewHolder(getLayoutInflater().inflate(R.layout.main_item, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MainViewHolder holder, int position) {
+            holder.bind(position);
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 15;
+        }
+    }
+
+    // Entenda como sendo a VIEW DA CELULA que está dentro da RecyclerView
+    private class MainViewHolder extends RecyclerView.ViewHolder {
+
+        public MainViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        public void bind(int position) {
+            TextView textTest = itemView.findViewById(R.id.textview_test);
+            textTest.setText("teste de rolagem" + position);
+        }
     }
 }
