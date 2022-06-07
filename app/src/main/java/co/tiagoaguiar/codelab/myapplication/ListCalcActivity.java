a3b120a959734e87df8de84589a7de003899afc4
luaraRamos <luara.m.ramos@gmail.com>
2022-06-07 14:40:20 -0300

    teste

--- app/src/main/java/co/tiagoaguiar/codelab/myapplication/ListCalcActivity.java
+++ app/src/main/java/co/tiagoaguiar/codelab/myapplication/ListCalcActivity.java
@@ -13,7 +13,7 @@ public class ListCalcActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_calc);
-
+        // teste
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             String type = extras.getString("type");
